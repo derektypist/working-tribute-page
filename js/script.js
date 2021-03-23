b879dc285bf72b40829a5d7bfb1001692d628bb7
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2021-03-23 16:19:28 +0000

    Updated object array to 1996 at script.js

--- js/script.js
+++ js/script.js
@@ -6,7 +6,9 @@ const timeline = [{
     "year": 1969,
     "milestone": "Born in Bromley, Kent",
     "url": "#"
-}, {
+}, 
+
+{
     "year": 1980,
     "milestone": "Attended First Horse Race Meeting in Epsom, Surrey",
     "url": "#"
@@ -15,5 +17,53 @@ const timeline = [{
 {
     "year": 1982,
     "milestone": "First holiday abroad in Belgium",
+    "url": "#"
+},
+
+{
+    "year": 1983,
+    "milestone": "First plane ride to Spain",
+    "url": "#"
+}, 
+
+{
+    "year": 1986,
+    "milestone": "Received 5 'O' Levels in Maths, Physics, Chemistry, Economics and Computer Studies",
+    "url": "#"
+},
+
+{
+    "year": 1987,
+    "milestone": "Obtained First Printer (Citizen LSP-10) for a computer (BBC Microcomputer)",
+    "url": "#"
+}, 
+
+{
+    "year": 1988,
+    "milestone": "Attended Thames Polytechnic (now University of Greenwich), London",
+    "url": "#"
+},
+
+{
+    "year": 1991,
+    "milestone": "Received a BSc in Maths, Statistics and Computing",
+    "url": "#"
+}, 
+
+{
+    "year": 1993,
+    "milestone": "Ran the mile in under 5 minutes for the first time",
+    "url": "https://www.bandbhac.org.uk/cr131-091993.html"
+},
+
+{
+    "year": 1994,
+    "milestone": "Passed Driving Test in Sidcup, Kent",
+    "url": "#"
+},
+
+{
+    "year": 1996,
+    "milestone": "Obtained First Windows PC",
     "url": "#"
 }];
