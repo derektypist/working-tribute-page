8f2cf83f3e12342b12ed686571d39e07148643fc
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2021-03-23 16:09:48 +0000

    Set up object array to 1982 at script.js

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Set Up Variable
+let txt = "";
+
+// Set Up Object Array with Keys of year, milestone and url
+const timeline = [{
+    "year": 1969,
+    "milestone": "Born in Bromley, Kent",
+    "url": "#"
+}, {
+    "year": 1980,
+    "milestone": "Attended First Horse Race Meeting in Epsom, Surrey",
+    "url": "#"
+},
+
+{
+    "year": 1982,
+    "milestone": "First holiday abroad in Belgium",
+    "url": "#"
+}];
